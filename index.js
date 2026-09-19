dfb784e8a2a09aa88ff05e2514187d544fa95739
Aashish Yadav <aashuyadav11000@gmail.com>
2026-09-19 22:58:45 +0530

    Updated counter app

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // document.getElementById("count-el").innertext = 5;
 
-let count = 0
+//let count = 5
+//count = count + 1
+//console.log(count)
 
-console.log(count)
+// Create a variable, bonusPoints. Initialize it as 50. Increase
+// decrease it down to 25, and then finally increase it to 70 
+// Console.log the value after each step
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
